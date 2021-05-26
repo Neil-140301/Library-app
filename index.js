3f94b90a3d1c6705ad189e69c5c7baef2051908c
Neil-140301 <alvares.1@iitj.ac.in>
2021-05-26 14:16:48 +0000

    added js functionality and css

--- index.js
+++ index.js
@@ -1,13 +1,132 @@
 let addBookBtn = document.getElementById('addBook');
+let formEl = document.getElementById('addBookForm');
+let titleInputEl = document.getElementById('titleInput');
+let authorInputEl = document.getElementById('authorInput');
+let pagesInputEl = document.getElementById('pagesInput');
+let readBookEl = document.getElementById('readBook');
+let booksContainerEl = document.getElementById('booksContainer');
 
-function addBook() {
-  console.log(addBookBtn);
+let myLibrary=getLibraryArray();
+let title,author,pages,readStatus;
+let bookCount=myLibrary.length;
+
+titleInputEl.addEventListener('change',event => {
+  title = event.target.value;
+});
+
+authorInputEl.addEventListener('change',event => {
+  author = event.target.value;
+});
+
+pagesInputEl.addEventListener('change',event => {
+  pages = event.target.value;
+});
+
+readBookEl.addEventListener('change',event => {
+  readStatus = event.target.checked;
+});
+
+function getLibraryArray() {
+    let tdl = localStorage.getItem('myLibraryArray');
+    let parsedLibraryList = JSON.parse(tdl);
+    return (parsedLibraryList === null)? []: parsedLibraryList;
+}
+
+function shadowClass(id){
+  switch (id%4) {
+    case 1:
+      return "shadow-bottom";
+    case 2:
+      return "shadow-left";
+    case 3:
+      return "shadow-top";
+    default:
+      return "shadow-right";
+  }
+}
+
+function deleteBook(bookId){
+  let book = document.getElementById(`book${bookId}`);
+  let bookIndex = myLibrary.findIndex(item => item.id === bookId);
+  myLibrary.splice(bookIndex,1);
+  booksContainerEl.removeChild(book);
+  localStorage.setItem('myLibraryArray',JSON.stringify(myLibrary));
+}
+
+function createAndAppendBook(bookObj){
+  const {id, title, author, noOfPages, hasRead} = bookObj;
+  let bookId = `book${id}`;
+
+  let bookCard = document.createElement('div');
+  bookCard.classList.add('books-card', shadowClass(id));
+  bookCard.id = bookId;
+  booksContainerEl.appendChild(bookCard);
+
+  let titleEl = document.createElement('h1');
+  titleEl.classList.add('card-title');
+  titleEl.textContent = title;
+  bookCard.appendChild(titleEl);
+
+  let authorEl = document.createElement('p');
+  authorEl.classList.add('card-author');
+  authorEl.textContent = author;
+  bookCard.appendChild(authorEl);
+
+  let pagesEl = document.createElement('p');
+  pagesEl.classList.add('card-pages');
+  pagesEl.textContent = "Pages : ";
+  bookCard.appendChild(pagesEl);
+
+  let noOfPagesEl = document.createElement('span');
+  noOfPagesEl.classList.add('pages-number');
+  noOfPagesEl.textContent = noOfPages;
+  pagesEl.appendChild(noOfPagesEl);
+
+  let readBookEl = document.createElement('p');
+  readBookEl.textContent = "Read the Book : ";
+  bookCard.appendChild(readBookEl);
+
+  let readStatusEl = document.createElement('button');
+  readStatusEl.classList.add('read-book',`${hasRead}`);
+  readStatusEl.textContent = hasRead? 'Y' : 'N';
+  readBookEl.appendChild(readStatusEl);
+
+  let deleteEl = document.createElement('button');
+  deleteEl.classList.add('delete-book');
+  bookCard.appendChild(deleteEl);
+
+  deleteEl.addEventListener('click',() => {
+    deleteBook(id);    
+  });
+
+  let icon = document.createElement('i');
+  icon.classList.add('fas','fa-times');
+  deleteEl.appendChild(icon);
 }
 
 //constructor function
 function Book(title,author,pages,read = false) {
+  this.id = bookCount + 1;  
   this.title = title;
   this.author = author;
   this.noOfPages = pages;
   this.hasRead = read;
+  bookCount += 1;
 }
+
+for (let book of myLibrary){
+  createAndAppendBook(book);
+}
+
+formEl.addEventListener('submit',event => {
+  event.preventDefault();
+  let newBook = new Book(title,author,pages,readStatus);
+  myLibrary.push(newBook);
+  localStorage.setItem('myLibraryArray',JSON.stringify(myLibrary));
+  createAndAppendBook(newBook);
+
+  titleInputEl.value = "";
+  authorInputEl.value = "";
+  pagesInputEl.value = "";
+  readBookEl.checked = false;
+});
